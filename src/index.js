3b2ba1d280e9e45dcae9f042eef92c7597d0ea5f
swieradmateusz <96368394+svieruss@users.noreply.github.com>
2023-06-10 13:56:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const app = require("./app");
-const port = process.env.PORT || 3000;
+const app = require("./app")
+const port = process.env.PORT || 3000
 
 app.listen(port, () =>
-	console.log('Nasłuchuje na porcie 3000');
+	console.log("Aplikacja nasłuchuje na porcie 3000"),
 );
